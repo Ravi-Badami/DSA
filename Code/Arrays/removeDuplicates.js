db0bb27bf9785f3c9ad3a837ebb66cc1425e257a
Ravikumar Badami <ravibadami191@gmail.com>
2026-02-20 23:43:46 +0530

    remove duplicates

---
+++ Code/Arrays/removeDuplicates.js
@@ -0,0 +1,28 @@
+// Brute force
+// Time-complexity = O(n)
+// space complexity = O(n)
+//let arr=[1,1,2,2,3,3,4,4];
+//
+//let index=0;
+//const set=new Set();
+//for(let num of arr){
+//if(!set.has(num)){
+//set.add(num);
+//set[index]=num;
+//index++;
+//}
+//}
+//console.log(set);
+
+
+
+--------------------------------------------------------------
+let arr=[1,1,2,2,3,3,4,4];
+let arr2=[];
+for(let i=0;i<arr.length;i++){
+
+if(arr[i])
+
+}
+
+
--- Code/script.js
+++ Code/script.js
@@ -4,8 +4,12 @@ let script = document.createElement('script');
 // script.src = './Arrays/twoSum/bruteForce.js';
 //script.src = './Arrays/twoSum/twoPhaseHashTable.js';
 //script.src = './Arrays/twoSum/onePhaseHashTable.js';
+//script.src = './Arrays/ifArraySortedOrNot.js';
+//script.src = './Arrays/ifArraySortedOrNot.js';
 //script.src = './Stacks/basicStack.js';
 //script.src = './Stacks/commandsStack.js';
 // script.src = './Stacks/currentTopStack.js';
-script.src = './Arrays/ifArraySortedOrNot.js';
+script.src = './Arrays/removeDuplicates.js';
+
+
 document.head.appendChild(script);
