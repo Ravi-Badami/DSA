86eaeee9d3463c35f9b4e3cd72a7b9b3d3ba902a
Ravikumar Badami <ravibadami191@gmail.com>
2026-04-22 23:13:13 +0530

    dsa practice

--- Code/Arrays/Striver/moveAllZerosToTheEndOfTheArray.js
+++ Code/Arrays/Striver/moveAllZerosToTheEndOfTheArray.js
@@ -1,5 +1,29 @@
 //let arr=[1,0,2,3,0,4,0,1];
+
+
+//Bruteforce
+// T = O(n) and S=O(n)
+//function bruteForce(arr){
+//   let temp=[];
+//    let j=0;
+//for(let i=0;i<arr.length;i++){
+//   if(arr[i]>0) {
+//       temp[j]=arr[i];
+//       j++;
+//   }
+//}
+//    for(let i=0;i<arr.length;i++){
+//        if(temp[i]===undefined){
+//            temp[i]=0;
+//        }
+//    }
+//    console.log(temp);
 //
+//}
+//bruteForce(arr);
+
+
+
 //function solution(nums){
 //let j=-1;
 //for(let i=0;i<nums.length;i++){
