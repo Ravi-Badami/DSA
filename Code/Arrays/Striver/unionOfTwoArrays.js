c80912aa68372a3e010bcd05e424b48fd7900347
Ravikumar Badami <ravibadami191@gmail.com>
2026-04-25 23:33:51 +0530

    union of two arrays

--- Code/Arrays/Striver/unionOfTwoArrays.js
+++ Code/Arrays/Striver/unionOfTwoArrays.js
@@ -6,43 +6,49 @@ function union(arr1,arr2,n,m){
 let i=0,j=0;
 let result=[];
 
+
+ // Iterate while both pointers are within array bounds
 while(i<n && j<m){
-if(arr1[i]<arr2[j]){
-if(result.length===0 || result[result.length-1]!==arr1[i]){
-result.push(arr1[i]);
-}
-i++;
+    // If element in arr1 is smaller
+    if(arr1[i]<arr2[j]){
+        // Add if empty or not duplicate
+        if(result.length===0 || result[result.length-1]!==arr1[i]){
+        result.push(arr1[i]);
 
-}else if(arr1[i]>arr2[j]){
-if(result.length===0 || result[result.length-1]!==arr2[j]){
-result.push(arr2[j]);
+    }
+        i++;
+    }
+    // If element in arr2 is smaller
+    else if(arr1[i]>arr2[j]){
+        // Add if empty or not duplicate
+        if(result.length===0 || result[result.length-1]!==arr2[j]){
+            result.push(arr2[j]);
+        }
+        j++;
+    }
+    else{
+        // Elements are equal, add once if not duplicate
+        if(result.length===0 || result[result.length-1]!==arr1[i]){
+            result.push(arr1[i]);
+        }
+        i++;
+        j++;
+    }
 }
-j++;
-
-}
-else{
-if(result.length===0 || result[result.length-1]!==arr1[i]){
-result.push(arr1[i]);
-}
-i++;
-j++;
-}
-}
-
+// Append remaining elements from arr1
 while(i<n){
-if(result[result.length-1]!==arr1[i]){
-result.push(arr1[i]);
-}
-i++;
+    if(result[result.length-1]!==arr1[i]){
+        result.push(arr1[i]);
+    }
+    i++;
 
 }
-
+// Append remaining elements from arr2
 while(j<m){
-if(result[result.length-1]!==arr2[j]){
-result.push(arr2[j]);
-
-}
-j++;
+    if(result[result.length-1]!==arr2[j]){
+      result.push(arr2[j]);
+    }
+    j++;
 }
 
 console.log(result);
