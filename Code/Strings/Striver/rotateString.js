52cd9e4e89d7e39fe12325562a23b4fc0046c6b0
Ravikumar Badami <ravibadami191@gmail.com>
2026-04-27 16:18:06 +0530

    rotate string{

--- Code/Strings/Striver/rotateString.js
+++ Code/Strings/Striver/rotateString.js
@@ -5,10 +5,10 @@
  */
 var rotateString = function(s, goal) {
     let combination=s+s;
-    let j=s.length;
+    let j=s.length-1;
     if(s.length!==goal.length) return false;
     for(let i=0;i<combination.length;i++){
-        let word=combination.substring(i,j);
+        let word=combination.substring(i,j+1);
         if(word===goal){
             return true;
         }else{
@@ -18,3 +18,5 @@ var rotateString = function(s, goal) {
     return false;
 
 };
+
+
